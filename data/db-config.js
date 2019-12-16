a4e59da523accb8795bd6f5316391cd8c8df7ad6
Thai Nguyen <discoverylight@yahoo.com>
2019-12-15 19:18:25 -0800

    staging experiment

--- data/db-config.js
+++ data/db-config.js
@@ -2,6 +2,6 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV || "staging";
 
 module.exports = knex(knexConfig[environment]);
