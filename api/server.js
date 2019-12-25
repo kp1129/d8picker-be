49f4c618504a348b4207a0de59cc9aab484075eb
Thai Nguyen <discoverylight@yahoo.com>
2019-12-25 14:36:47 -0800

    resolved merge conflict

--- api/server.js
+++ api/server.js
@@ -14,28 +14,19 @@ const authRouter = require("../auth/auth-router");
 const calendarRouter = require("../routes/calendar-routes");
 const eventRouter = require("../routes/event-routes");
 const invitationRouter = require("../routes/invitation-routes");
-<<<<<<< HEAD
 const colorRouter = require("../routes/color-routes");
 const twilioRouter = require("../routes/twilio-routes");
 const googleCalRouter = require("../routes/google-cal-api-routes");
-=======
-const twilioRouter = require("../routes/twilioMessage");
-
->>>>>>> ca10fa706030b0d552bb8deefb00df86023ca474
 //routes
 server.use("/users", userRouter);
 server.use("/auth", authRouter);
 server.use("/api/calendars/", calendarRouter);
 server.use("/api/events/", eventRouter);
 server.use("/api/invitations/", invitationRouter);
-<<<<<<< HEAD
 server.use("/api/colors", colorRouter);
 server.use("/api/twilio/", twilioRouter);
 server.use("/api/google/", googleCalRouter);
 swaggerDoc(server);
-=======
-server.use("/api/twilio/", twilioRouter);
->>>>>>> ca10fa706030b0d552bb8deefb00df86023ca474
 
 //testing server
 server.get("/", (req, res) => {
