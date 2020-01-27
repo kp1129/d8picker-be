923f83dffa0f6bc23d65e2da606c5819348cbfb2
Kennethaustin <kenneth.austin93@gmail.com>
2020-01-27 17:44:52 -0500

    starting routes and dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-
+require('dotenv/config');
 //ROUTES
 app.get('/',(req, res)=> {
     res.send('this is home');
@@ -10,8 +10,13 @@ app.get('/posts',(req,res) =>{
     res.send('this is posts');
 });
 
+//MIDDLEWARE
+
+
 //TESTING DB KENNETH
-mongoose.connect('mongodb+srv://testdb:testing123@cluster0-hnltr.gcp.mongodb.net/test?retryWrites=true&w=majority', () => {
+mongoose.connect(
+    'mongodb+srv://testdb:testing123@cluster0-hnltr.gcp.mongodb.net/test?retryWrites=true&w=majority',
+ { useNewUrlParser: true }, () => {
     console.log('connected to DB')
 })
 
