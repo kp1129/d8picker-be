ef2f54f727261ad72e368629c20739f000bb2e85
Kennethaustin <kenneth.austin93@gmail.com>
2020-01-27 19:05:14 -0500

    import route from posts

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const app = express();
 const mongoose = require('mongoose')
 require('dotenv/config');
 
+//Import Routes
+const postsRoutes = require('./router/posts');
+
+app.use('/posts', postsRoutes);
 
 //ROUTES
 app.get('/',(req, res)=> {
     res.send('this is home');
 });
-app.get('/posts',(req,res) =>{
-    res.send('this is posts');
-});
 
 //MIDDLEWARE
 
 
-//TESTING DB KENNETH
+//CONNECT DB
 mongoose.connect(
   "mongodb+srv://admin:yj8p6vLZjRE0zw4P@d8picker-db-qqopr.mongodb.net/test?retryWrites=true&w=majority",
   { useNewUrlParser: true },
