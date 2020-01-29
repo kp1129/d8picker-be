e4e64f6898c4e373dea1571ac08b8bdf905dac1b
juarezfrench <markedfrench@gmail.com>
2020-01-29 16:12:26 -0600

    Updated server.js for event route.

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 
 const authRoute = require("../routes/auth");
-
+const eventRoute = require("../routes/events");
 const server = express();
 
 server.use(helmet());
@@ -11,6 +11,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/user", authRoute);
+server.use("/api/event", eventRoute);
 
 server.get("/", (req, res) => {
   res.send({ api: "Ok" });
