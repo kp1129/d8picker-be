e12adbdabf3bfcc8f032113dbd0a0778d41d6b8a
juarezfrench <markedfrench@gmail.com>
2020-01-29 15:50:35 -0600

    Set up event schema.

---
+++ model/Event.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  eventName: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 64
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 120
+  },
+  location: {
+    type: String,
+    required: true,
+    max: 1024,
+    min: 6
+  },
+  color: {
+    type: String,
+    required: false,
+    min: 4,
+    max: 64
+  }
+});
+
+module.exports = mongoose.model("event", userSchema);
