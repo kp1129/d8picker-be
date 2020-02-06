6eaaf9b8a04789f3d810501330d8b011c319df54
DeAndre Johnson <dre@phreshr.com>
2020-02-07 05:47:29 +0800

    move token to google endpoint, add calendar scope

--- routes/auth.js
+++ routes/auth.js
@@ -12,18 +12,22 @@ router.get(
   "/google",
   passport.authenticate("google", {
     session: false,
-    scope: ["profile", "email"]
-  })
+    scope: [
+      "profile",
+      "email",
+      "https://www.googleapis.com/auth/calendar.events"
+    ]
+  }),
+  (req, res) => {
+    const token = jwt.sign({ id: req.user._id }, process.env.TOKEN_SECRET);
+    res.status(200).json({ token });
+  }
 );
 
 // callback route for google to redirect to
 router.get(
   "/google/redirect",
-  passport.authenticate("google", { session: false }),
-  (req, res) => {
-    const token = jwt.sign({ id: req.user._id }, process.env.TOKEN_SECRET);
-    res.status(200).json({ token });
-  }
+  passport.authenticate("google", { session: false })
 );
 
 module.exports = router;
