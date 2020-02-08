2d9c0a62fae3e13eeef02d9ee491922181b07caa
juarezfrench <markedfrench@gmail.com>
2020-02-07 18:00:54 -0600

    Comments on server.js

--- api/server.js
+++ api/server.js
@@ -1,30 +1,40 @@
+//Setup middleware
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-// require("../middleware/passport");
+
+//Pull in routes
 const authRoute = require("../routes/auth");
 const profileRoute = require("../routes/profile");
+
+//Outside libraries
 const cookieSession = require('cookie-session');
-require("dotenv").config();
 const passport = require('passport');
 
+//Require env variables
+require("dotenv").config();
 const server = express();
 
+//Invoke middleware
 server.use(helmet());
 server.use(cors());
-server.use(express.json());
+server.use(express.json()); 
+
+// Set cookie with 24hr session
 server.use(cookieSession({
   maxAge: 24*60*60*1000,
   keys:[process.env.SESSION_KEYS]
 }))
 
+//Initialize passport session
 server.use(passport.initialize());
 server.use(passport.session())
 
-
+//Invoke routes
 server.use("/api/auth", authRoute);
 server.use("/api/user", profileRoute);
 
+//GET endpoint for checking app
 server.get("/", (req, res) => {
   res.send({ api: "Ok", dbenv: process.env.DB_ENV });
 });
