a664303459797c7d3a9aa30d1f6ce800f0070b95
DeAndre Johnson <dre@phreshr.com>
2020-02-10 23:30:14 +0800

    add events endpoint

---
+++ routes/events.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { google } = require("googleapis");
+const googleCalenderService = require("../services/google-calendar.service");
+
+const router = express.Router();
+
+// Get Events
+router.get("/", async (req, res) => {
+  // check for valid session
+  if (req.session.user) {
+    // get oauth2 client
+    const oauth2Client = new google.auth.OAuth2();
+    oauth2Client.setCredentials({
+      access_token: req.session.user.accessToken
+    });
+
+    // get calendar events by passing oauth2 client
+    googleCalenderService.listEvents(oauth2Client, events => {
+      console.log(events);
+
+      const data = {
+        ...req.session.user,
+        events: events
+      };
+      console.log(data);
+      res.json(data);
+    });
+  } else {
+    // Should check for session on front end and redirect
+    res.json({ message: "Not Authorized" });
+  }
+});
+
+module.exports = router;
