706e2d5f7040d477f3332b4d946b8b329a368795
DeAndre Johnson <dre@phreshr.com>
2020-02-18 22:52:29 +0800

    rename to setSession since cookies aren't used

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const googleUtil = require("../utils/google-util");
-const setCookie = require("../middleware/setCookie");
+const express = require('express');
+const googleUtil = require('../utils/google-util');
+const setSession = require('../middleware/setSession');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const router = express.Router();
 
 // Redirect for authentication uri
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
   res.redirect(googleUtil.urlGoogle());
 });
 
 // callback route for google redirect
-router.get("/success", setCookie, (req, res) => {
+router.get('/success', setSession, (req, res) => {
   // TODO: Should check if needed
   // TODO: change to environment variable
-  res.redirect("http://localhost:3000/redirect");
+  res.redirect(`${process.env.FRONTEND_URL}/redirect`);
 });
 
 // Delete session and logout
 // Should redirect on the front end
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
       console.log(err);
       // res.redirect("/");
     }
-    res.clearCookie("sid");
+    res.clearCookie('sid');
     // res.redirect("/");
   });
 });
