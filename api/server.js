8a8148aba0d47082ea320ecbd9b05c7629ce5f46
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-03-04 10:25:46 -0800

    Remove space from server.

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const templateRoute = require('../routes/template');
 
-
 //Require env variables
 require('dotenv').config();
 const server = express();
