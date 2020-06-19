cc2766173bccdfeac85b491b1b299dbfb8c61fdb
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-06-19 09:17:26 -0700

    configd pg in knexfile

--- knexfile.js
+++ knexfile.js
@@ -60,18 +60,33 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./db/migrations",
+    },
   }
 
 };
+
+
+
+//-----
+// production: {
+//   client: "pg",
+//   connection: process.env.DATABASE_URL,
+//   pool: {
+//     min: 2,
+//     max: 10,
+//   },
+//   useNullAsDefault: true,
+//   migrations: {
+//     directory: "./data/migrations",
+//   },
+//   seeds: {
+//     directory: "./data/seeds",
+//   },
+// },
