c51e96abccfca320659520c8c0710d04888f5274
Vanshika <58994654+VanshikaP@users.noreply.github.com>
2020-06-19 09:54:55 -0700

    Update groupsModel.js

--- model/groupsModel.js
+++ model/groupsModel.js
@@ -24,6 +24,7 @@ function findGroupsByAdminId(adminId){
 function addGroup(newGroupInfo){
     return db('groups')
             .insert(newGroupInfo)
+            .returning(["id"])
 }
 
 // find group by groupId
