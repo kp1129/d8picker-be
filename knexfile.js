8bda414596cd3b219b34f237c6a54c0e0bb89dfa
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-06-23 15:37:53 -0400

    made changes to hopefully fix cors issues

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,40 @@
 // Update with your config settings.
+require("dotenv").config();
+const pg = require("pg");
+pg.defaults.ssl = { rejectUnauthorized: false };
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './db/d8picker.db3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-      }
-    }
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
   },
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './db/d8picker.db3'
+  //   },
+  //   useNullAsDefault: true,
+  //   migrations: {
+  //     directory: './db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './db/seeds'
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       // runs after a connection is made to the sqlite engine
+  //       conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+  //     }
+  //   }
+  // }
 
   testing: {
     client: 'sqlite3',
@@ -59,7 +73,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
